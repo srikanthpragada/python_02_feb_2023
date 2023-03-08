087d0d8c4168626eba0aa3b4cfa947d6d3d2ac5c
srikanthpragada <srikanthpragada@gmail.com>
2023-03-08 20:57:08 +0530

    08-MAR

--- demo/oop/doctors.py
+++ demo/oop/doctors.py
@@ -1,5 +1,8 @@
-# Superclass
-class Doctor:
+from abc import abstractmethod, ABC
+
+
+# Abstract class
+class Doctor(ABC):
     def __init__(self, name, dept):
         self.name = name
         self.dept = dept
@@ -10,6 +13,10 @@ class Doctor:
 
     def getdept(self):
         return self.dept
+
+    @abstractmethod
+    def getsalary(self):
+        pass
 
 
 # Subclass
@@ -27,5 +34,27 @@ class ResidentDoctor(Doctor):
         return self.salary
 
 
+class Consultant(Doctor):
+    def __init__(self, name, dept, visits, charge):
+        super().__init__(name, dept)
+        self.visits = visits
+        self.charge = charge
+
+    # Override show() of superclass
+    def show(self):
+        super().show()
+        print("Visits  : ", self.visits)
+        print("Charge  : ", self.charge)
+
+    def getsalary(self):
+        return self.visits * self.charge
+
+
 r = ResidentDoctor("Dr. Joe", "Card", 300000)
 r.show()
+print(r.getsalary())
+c = Consultant("Dr. Jack", "Ped", 10, 1000)
+c.show()
+print(c.getsalary())
+
+d = Doctor("","")
